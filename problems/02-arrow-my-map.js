5ee8da9206e73d6b8e65bae55d9cc557dad3210b
Talazendah <talazendah@gmail.com>
2022-07-14 18:04:06 -0500

    02 For loop version

--- problems/02-arrow-my-map.js
+++ problems/02-arrow-my-map.js
@@ -1,7 +1,7 @@
 /***********************************************************************
 Write a function using fat arrow syntax named `arrowMyMap` that accepts an array
 and a callback as arguments. The function will return an array of new elements
-obtained by calling the callback on each element of the array, passing in the 
+obtained by calling the callback on each element of the array, passing in the
 element. Assign the below function to a variable using the const keyword.
 
 Do not use the built in Array#map - use Array#forEach for iteration.
@@ -18,8 +18,25 @@ console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 ***********************************************************************/
 
-// your code here!
+/*const arrowMyMap = (arr, cb) => {
+  let finalArray = [];
+    arr.forEach(el => finalArray.push(cb(el)));
 
+  return finalArray;
+} */
+
+const arrowMyMap = (arr, cb) => {
+  let finalArray = [];
+  for (let i = 0; i < arr.length; i++ ){
+    let tempArr = arr[i];
+    let tempArr2 = cb(tempArr)
+    finalArray.push(tempArr2);
+  }
+  
+  return finalArray;
+}
+let result1 = arrowMyMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
